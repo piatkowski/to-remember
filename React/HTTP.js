fddc1660c66f140bdef5dfc87f10d9781d6f004b
piatkowski <piatkowski92@gmail.com>
2022-09-01 13:31:30 +0200

    Update HTTP.js

--- React/HTTP.js
+++ React/HTTP.js
@@ -1,1 +1,41 @@
+/**
+* Example of GET request width Fetch API
+*/
 
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+    const [movies, setMovie] = useState([]);
+
+    function fetchMoviesHandler() {
+        fetch('https://swapi.dev/api/films').then(response => {
+            return response.json();
+        }).then(data => {
+            const transformedMovies = data.results.map(movieData => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date
+                }
+            });
+            setMovie(transformedMovies);
+        });
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                <MoviesList movies={movies}/>
+            </section>
+        </React.Fragment>
+    );
+}
+
+export default App;
