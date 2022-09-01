e38ef94d5a4fcee0db942c2849004d04cdcfeaa7
piatkowski <piatkowski92@gmail.com>
2022-09-01 13:43:45 +0200

    Update HTTP.js

--- React/HTTP.js
+++ React/HTTP.js
@@ -25,7 +25,29 @@ function App() {
             setMovie(transformedMovies);
         });
     }
+    
+    
+    /**
+    * async/await alternative
+    */
+    
+        async function fetchMoviesHandler() {
+            const response = await fetch('https://swapi.dev/api/films');
+            const data = await response.json();
+            const transformedMovies = data.results.map(movieData => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date
+                }
+            });
+            setMovies(transformedMovies);
+        }
 
+    /**
+    * return component's JSX code
+    */
     return (
         <React.Fragment>
             <section>
